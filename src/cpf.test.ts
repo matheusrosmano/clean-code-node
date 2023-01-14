e499dcec7fa5f3ade93a0e5354f284070602f4f6
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-01-14 12:09:55 -0300

    fix: Rename file

--- src/cpf.test.ts
+++ src/cpf.test.ts
@@ -1,7 +1,7 @@
 import test from 'node:test'
 import { isAssertClause } from 'typescript'
 import * as assert from "assert";
-import {validate} from "./cpf-before";
+import {validate} from "./cpf";
 
 test("Valid cpf with mask", (t) => {
     assert.strictEqual(validate("912.785.830-84"), true)
