4b378d0dc27c03eaca619b5a2e86b95840ab6e0d
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-01-22 00:32:33 -0300

    fix: Clean code of cpf

--- src/cpf.ts
+++ src/cpf.ts
@@ -1,44 +1,36 @@
 export class CPF {
-  private cpf: string
+  private cpf: string;
 
   constructor(cpf: string) {
-      if (cpf === null || cpf === undefined) new Error("CPF isn't informed")
-      if (cpf.length !== 11 && cpf.length !== 14) new Error("CPF length: 11 or 14")
-      this.cpf = cpf
+    if (cpf === null || cpf === undefined) new Error("CPF isn't informed");
+    if (cpf.length !== 11 && cpf.length !== 14) new Error("CPF length: 11 or 14");
+    if (cpf.split("").every(c => c === cpf[0])) new Error("Invalid cpf")
+    this.cpf = cpf;
   }
 
   isValid(): boolean {
     const cpf: string = this.cpf.replace(/\D/g, "");
 
-      try {
-          let d1, d2;
-          let primeiroDigitoVerificador, segundoDigitoVerificador, rest;
-          let numeroSemDigitoVerificador;
-          d1 = d2 = 0;
-          primeiroDigitoVerificador = segundoDigitoVerificador = rest = 0;
+    let d1 = 0
+    let d2 = 0
 
-          for (let indiceCpf = 1; indiceCpf < cpf.length - 1; indiceCpf++) {
+    for (let indiceCpf = 1; indiceCpf < cpf.length - 1; indiceCpf++) {
+      let numeroSemDigitoVerificador: number = parseInt(cpf.substring(indiceCpf - 1, indiceCpf));
+      d1 = d1 + (11 - indiceCpf) * numeroSemDigitoVerificador;
+      d2 = d2 + (12 - indiceCpf) * numeroSemDigitoVerificador;
+    }
 
-          numeroSemDigitoVerificador = parseInt(cpf.substring(indiceCpf - 1, indiceCpf));
-          d1 = d1 + (11 - indiceCpf) * numeroSemDigitoVerificador;
+    let restoDivisao = d1 % 11;
 
-          d2 = d2 + (12 - indiceCpf) * numeroSemDigitoVerificador;
-          }
+    let primeiroDigitoVerificador: number = restoDivisao < 2 ? 0 : 11 - restoDivisao;
+    d2 += 2 * primeiroDigitoVerificador;
+    restoDivisao = d2 % 11;
+    let segundoDigitoVerificador: number = (restoDivisao < 2) ? 0 : 11 - restoDivisao;
 
-          rest = d1 % 11;
+    let digitosVerificadores = cpf.substring(cpf.length - 2, cpf.length);
+    const concatenaDigitosVerificadores: string = `${primeiroDigitoVerificador}${segundoDigitoVerificador}`;
+    if (digitosVerificadores !== concatenaDigitosVerificadores) new Error();
 
-          primeiroDigitoVerificador = rest < 2 ? (primeiroDigitoVerificador = 0) : 11 - rest;
-          d2 += 2 * primeiroDigitoVerificador;
-          rest = d2 % 11;
-          if (rest < 2) segundoDigitoVerificador = 0;
-          else segundoDigitoVerificador = 11 - rest;
-
-          let digitosVerificadores = cpf.substring(cpf.length - 2, cpf.length);
-          const concatenaDigitosVerificadores: string = `${primeiroDigitoVerificador}${segundoDigitoVerificador}`
-          if (digitosVerificadores !== concatenaDigitosVerificadores) new Error()
-      } catch (e) {
-          throw "Invalid cpf"
-      }
-      return true
+    return true;
   }
 }
